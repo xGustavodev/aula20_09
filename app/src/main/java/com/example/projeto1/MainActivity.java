8c821596fdc21d6e5f7d6eafb635ad5ca4f0a762
xGustavodev <gustavo.viviani@hotmail.com>
2023-09-20 21:14:31 -0300

    Mudar de tela

--- app/src/main/java/com/example/projeto1/MainActivity.java
+++ app/src/main/java/com/example/projeto1/MainActivity.java
@@ -2,32 +2,43 @@ package com.example.projeto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextNumber4;
+
     private EditText editTextNumber5;
 
     private EditText editTextNumber6;
-
-    private EditText editTextNumber7;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextNumber4 = findViewById(R.id.editTextNumber4);
         editTextNumber5 = findViewById(R.id.editTextNumber5);
         editTextNumber6 = findViewById(R.id.editTextNumber6);
-        editTextNumber7 = findViewById(R.id.editTextNumber7);
     }
 
     public void somar(View view) {
         int x,y,soma;
-        x = Integer.parseInt(editTextNumber5.getText().toString());
-        y = Integer.parseInt(editTextNumber6.getText().toString());
+        x = Integer.parseInt(editTextNumber4.getText().toString());
+        y = Integer.parseInt(editTextNumber5.getText().toString());
         soma = x + y;
-        editTextNumber7.setText(Integer.toString(soma));
+        editTextNumber6.setText(Integer.toString(soma));
+    }
+    public void abrirJanelaNova( View view) {
+        int x,y, soma;
+        x = Integer.parseInt(editTextNumber4.getText().toString());
+        y = Integer.parseInt(editTextNumber5.getText().toString());
+        soma = x + y;
+
+        Intent packageContext;
+        Intent intent = new Intent( this, MainActivity2.class);
+        startActivity(intent);
     }
 }
